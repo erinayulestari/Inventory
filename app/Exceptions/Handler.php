848b8e8615ec006ed5d0711219d89edc84d6b794
Erin Ayu Lestari <erinayulestari09@gmail.com>
2026-05-22 02:31:26 +0800

    Implement validation, error handling & service layer

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,28 +3,24 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException; 
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+    // ... kode lainnya ...
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        $this->renderable(function (ValidationException $e) {
+            return response()->json([
+                'status'  => 'error',
+                'data'    => null,
+                'message' => collect($e->errors())->flatten()->first(),
+            ], 500);
         });
     }
 }
